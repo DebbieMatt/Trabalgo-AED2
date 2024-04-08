1d9ab553a4c1356575b1a989f864d1c1122055da
m4vi <vitlia40@gmail.com>
2024-04-08 01:15:18 -0400

    ujj

--- main.c
+++ main.c
@@ -1,32 +1,40 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct {
+typedef struct
+{
     char nome[50];
     int idade;
+    int prioridade; // 1 Azul 2 Amarelo 3 Vermelho
 } PACIENTE;
 
-typedef struct {
+typedef struct
+{
     char nome[40];
     char especialidade[50];
+    // PACIENTE *pacientes;
 } MEDICO;
 
-//Estrutura para uma lista encadeada
-typedef struct ITEM {
+// Estrutura para uma lista encadeada
+typedef struct ITEM
+{
     void *chave;
     struct ITEM *prox;
 } ITEM;
 
-typedef struct {
+typedef struct
+{
     ITEM *inicio;
     int qtde;
 } LISTA;
 
-//Função para cadastrar paciente
-PACIENTE *cadastrarPaciente() {
+// Função para cadastrar paciente
+PACIENTE *cadastrarPaciente()
+{
     PACIENTE *novoPaciente = (PACIENTE *)malloc(sizeof(PACIENTE)); // Alocando memória
-    if (novoPaciente == NULL) {
+    if (novoPaciente == NULL)
+    {
         printf("Erro ao alocar memoria!\n");
         return NULL;
     }
@@ -36,17 +44,23 @@ PACIENTE *cadastrarPaciente() {
     fgets(novoPaciente->nome, sizeof(novoPaciente->nome), stdin);
     novoPaciente->nome[strcspn(novoPaciente->nome, "\n")] = '\0';
 
-    printf("Idade: "); 
+    printf("Idade: ");
     scanf("%d", &novoPaciente->idade);
+    getchar(); // Limpa o buffer
+
+    printf("Prioridade: ");
+    scanf("%d", &novoPaciente->prioridade);
     getchar(); // Limpa o buffer
 
     return novoPaciente;
 }
 
-//Função para cadastrar médico
-MEDICO *cadastrarMedico() {
+// Função para cadastrar médico
+MEDICO *cadastrarMedico()
+{
     MEDICO *novoMedico = (MEDICO *)malloc(sizeof(MEDICO)); // Alocando memória
-    if (novoMedico == NULL) {
+    if (novoMedico == NULL)
+    {
         printf("Erro ao alocar memoria!\n");
         return NULL;
     }
@@ -63,12 +77,43 @@ MEDICO *cadastrarMedico() {
     return novoMedico;
 }
 
-int main() {
+void bubbleSortPrioridadeCrescente(PACIENTE v[], int tam)
+{
+    for (int i = 0; i < tam - 1; i++)
+    {
+        for (int j = 0; j < tam - i - 1; j++)
+        {
+            if (v[j].prioridade > v[j + 1].prioridade)
+            {
+                // trocar prioridade
+                int aux = v[j].prioridade;
+                v[j].prioridade = v[j + 1].prioridade;
+                v[j + 1].prioridade = aux;
+
+                // trocar idades correspondente a prioridade
+                aux = v[j].idade;
+                v[j].idade = v[j + 1].idade;
+                v[j + 1].idade = aux;
+
+                // troca nomes correspondentes a prioridade
+                char auxc[50];
+                strcpy(auxc, v[j].nome);
+                strcpy(v[j].nome, v[j + 1].nome);
+                strcpy(v[j + 1].nome, auxc);
+            }
+        }
+    }
+}
+
+
+int main()
+{
     PACIENTE *novoPaciente;
     MEDICO *novoMedico;
     int op;
-
-    do {
+    int quant_paciente = 0;
+    do
+    {
         printf("----- SISTEMA DE PRONTO ATENDIMENTO -----\n");
         printf("1 - Cadastrar paciente\n");
         printf("2 - Cadastrar medico\n");
@@ -82,36 +127,60 @@ int main() {
         scanf("%d", &op);
         getchar(); // Limpa o buffer de entrada após o scanf
 
-        switch (op) {
-            case 1:
-                novoPaciente = cadastrarPaciente();
+        switch (op)
+        {
+        case 1:
+            novoPaciente = cadastrarPaciente();
+            quant_paciente++;
+            break;
+        case 2:
+            novoMedico = cadastrarMedico();
+            break;
+        case 3:
+            printf("Lista de Pacientes: \n");
+            // Função listar pacientes
+            printf("Deseja Listar Paciente em ordem de 1-nome, 2-idade, 3-prioridade? ");
+            int aux;
+            scanf("%d", &aux);
+            switch (aux)
+            {
+            case 1: // nome
+                /* code */
                 break;
-            case 2:
-                novoMedico = cadastrarMedico();
+            case 2: // idade
                 break;
-            case 3:
-                printf("Lista de Pacientes: \n");
-                //Função listar pacientes
+            case 3: // prioridade
+                bubbleSortPrioridadeCrescente(novoPaciente, quant_paciente);
+                for (int i = 0; i < quant_paciente; i++)
+                {
+                    printf("Prioridade: %d. Paciente: %s \n",novoPaciente->prioridade, novoPaciente->nome);
+                }
+                
                 break;
-            case 4:
-                printf("Lista de Medicos: \n");
-                //Função para listar médicos
-                break;
-            case 5:
-                //Realizar atendimentos
-                break;
-            case 6:
-                //listar atendimentos
-                break;
-            case 7:
-                //cancelar atendimentos
-                break;
-            case 0:
-                printf("Saindo do sistema\n");
-                exit(0);
+
             default:
-                printf("Opcao invalida!\n");
                 break;
+            }
+            break;
+        case 4:
+            printf("Lista de Medicos: \n");
+            // Função para listar médicos
+            break;
+        case 5:
+            // Realizar atendimentos
+            break;
+        case 6:
+            // listar atendimentos
+            break;
+        case 7:
+            // cancelar atendimentos
+            break;
+        case 0:
+            printf("Saindo do sistema\n");
+            exit(0);
+        default:
+            printf("Opcao invalida!\n");
+            break;
         }
     } while (op != 0);
 
