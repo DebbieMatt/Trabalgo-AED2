fab1883c46109288d14e2ecca39e151fcfa08d1e
m4vi <vitlia40@gmail.com>
2024-04-11 13:34:44 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,95 @@
-#include "funcoes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    char nome[50];
+    int idade;
+} PACIENTE;
+
+typedef struct
+{
+    char nome[40];
+    char especialidade[50];
+} MEDICO;
+
+typedef struct
+{
+    PACIENTE *paciente;
+    MEDICO *medico;
+    char motivo[50];
+    int prioridade;
+} ATENDIMENTO;
+
+// Estrutura para uma lista encadeada
+typedef struct ITEM
+{
+    void *chave;
+    struct ITEM *prox;
+} ITEM;
+
+typedef struct
+{
+    ITEM *inicio;
+    int qtde;
+} LISTA;
+
+// Inicializa a lista
+void inicializa(LISTA *l)
+{
+    l->inicio = NULL;
+    l->qtde = 0;
+}
+
+// Insere um elemento na lista
+void inserir(LISTA *l, void *chave)
+{
+    ITEM *novoItem = (ITEM *)malloc(sizeof(ITEM));
+    if (novoItem == NULL)
+    {
+        printf("Erro ao alocar memoria!\n");
+        return;
+    }
+
+    novoItem->chave = chave;
+    novoItem->prox = l->inicio;
+    l->inicio = novoItem;
+    l->qtde++;
+}
+
+// Imprime os dados do paciente
+void imprimirPaciente(void *chave)
+{
+    if (chave != NULL)
+    {
+        PACIENTE *p = (PACIENTE *)chave;
+        printf("Nome: %s\n", p->nome);
+        printf("Idade: %d\n", p->idade);
+    }
+}
+
+// Função para cadastrar paciente
+PACIENTE *cadastrarPaciente()
+{
+    PACIENTE *novoPaciente = (PACIENTE *)malloc(sizeof(PACIENTE)); // Alocando memória
+    if (novoPaciente == NULL)
+    {
+        printf("Erro ao alocar memoria!\n");
+        return NULL;
+    }
+
+    printf("\nCADASTRO DE PACIENTE\n");
+    printf("Nome: ");
+    fgets(novoPaciente->nome, sizeof(novoPaciente->nome), stdin);
+    novoPaciente->nome[strcspn(novoPaciente->nome, "\n")] = '\0';
+
+    printf("Idade: ");
+    scanf("%d", &novoPaciente->idade);
+    getchar(); // Limpa o buffer
+
+    return novoPaciente;
+}
 
 // Função de comparação para qsort
 int compararPacientes(const void *a, const void *b)
@@ -15,6 +106,55 @@ int compararPacientes(const void *a, const void *b)
 
     // Se os nomes são iguais, compare as idades
     return pacienteA->idade - pacienteB->idade;
+}
+
+// Função para listar pacientes cadastrados
+void listarPacientes(PACIENTE *p, int quantidade)
+{
+    if (p == NULL || quantidade == 0)
+    {
+        printf("Nenhum paciente cadastrado!\n");
+        return;
+    }
+
+    printf("\nPaciente Cadastrados: ");
+    for (int i = 0; i < quantidade; i++)
+    {
+        printf("\nNome: %s\n", p[i].nome);
+        printf("Idade: %d\n", p[i].idade);
+    }
+}
+
+void imprimirMedico(void *chave)
+{
+    if (chave != NULL)
+    {
+        MEDICO *m = (MEDICO *)chave;
+        printf("Nome: %s\n", m->nome);
+        printf("Especialidade: %s\n", m->especialidade);
+    }
+}
+
+// Função para cadastrar médico
+MEDICO *cadastrarMedico()
+{
+    MEDICO *novoMedico = (MEDICO *)malloc(sizeof(MEDICO)); // Alocando memória
+    if (novoMedico == NULL)
+    {
+        printf("Erro ao alocar memoria!\n");
+        return NULL;
+    }
+
+    printf("CADASTRO DE MEDICO\n");
+    printf("Nome: ");
+    fgets(novoMedico->nome, sizeof(novoMedico->nome), stdin);
+    novoMedico->nome[strcspn(novoMedico->nome, "\n")] = '\0';
+
+    printf("Especialidade: ");
+    fgets(novoMedico->especialidade, sizeof(novoMedico->especialidade), stdin);
+    novoMedico->especialidade[strcspn(novoMedico->especialidade, "\n")] = '\0';
+
+    return novoMedico;
 }
 
 // Função de comparação para qsort
@@ -34,24 +174,410 @@ int compararMedicos(const void *a, const void *b)
     return strcmp(medicoA->especialidade, medicoB->especialidade);
 }
 
-/*void trocar(int *a, int *b){
-    int aux = *a;
-    *a = *b;
-    *b = aux;
+// Função para listar os médicos cadastrados
+void listarMedicos(MEDICO *m, int quantidade)
+{
+    if (m == NULL || quantidade == 0)
+    {
+        printf("Nenhum medico cadastrado!\n");
+        return;
+    }
+
+    printf("\nMedicos Cadastrados:");
+    for (int i = 0; i < quantidade; i++)
+    {
+        printf("\nNome: %s\n", m[i].nome);
+        printf("Especialidade: %s\n", m[i].especialidade);
+    }
 }
 
-void bubbleSort(int v[], int tam){
-    for (int i = 0; i < tam - 1; i++){
-        bool trocou = false;
-        for (int j = 0; j < tam - i - 1; j++){
-            if (v[j] > v[j + 1]){
-                trocar(&v[j], &v[j+1]); 
-                trocou = true;
-            }
-        }
-        if(!trocou) break;
+void consulta(PACIENTE *p, int qtdeP, MEDICO *m, int qtdeM, LISTA *l)
+{
+    if (p == NULL || qtdeP == 0 || m == NULL || qtdeM == 0 || l == NULL)
+    {
+        printf("Nao ha nenhum cadastro no sistema!\n");
+        return;
     }
-}*/
+
+    char paciente[40];
+    char medico[40];
+    char motivo[50];
+    char especialidade[50];
+    int prioridade, opPaciente, opMedico;
+
+    printf("\nRealizar atendimento");
+
+    printf("\nPacientes cadastrados:\n");
+    for (int i = 0; i < qtdeP; i++)
+    {
+        printf("%d - %s\n", i + 1, p[i].nome);
+    }
+
+    printf("Informe o paciente: ");
+    scanf("%d", &opPaciente);
+    getchar();
+
+    if (opPaciente < 1 || opPaciente > qtdeP)
+    {
+        printf("Opcao invalida!\n");
+        return;
+    }
+    strcpy(paciente, p[opPaciente - 1].nome);
+
+    // Listar médicos cadastrados
+    printf("Medicos cadastrados:\n");
+    for (int i = 0; i < qtdeM; i++)
+    {
+        printf("%d - %s - %s\n", i + 1, m[i].nome, m[i].especialidade);
+    }
+
+    printf("Informe o medico: ");
+    scanf("%d", &opMedico);
+    getchar();
+
+    if (opMedico < 1 || opMedico > qtdeM)
+    {
+        printf("Opcao invalida!\n");
+        return;
+    }
+    strcpy(medico, m[opMedico - 1].nome);
+
+    printf("Motivo da consulta ");
+    fgets(motivo, sizeof(motivo), stdin);
+    motivo[strcspn(motivo, "\n")] = '\0';
+
+    printf("Prioridade (1 - leve, 2 - moderada, 3 - grave): ");
+    scanf("%d", &prioridade);
+    getchar();
+
+    // inicia um novo atendimento
+    ATENDIMENTO *novo = (ATENDIMENTO *)malloc(sizeof(ATENDIMENTO));
+    if (novo == NULL)
+    {
+        printf("Erro ao alocar memoria!\n");
+        return;
+    }
+
+    novo->paciente = &p[opPaciente - 1];
+    novo->medico = &m[opMedico - 1];
+    strcpy(novo->motivo, motivo);
+    novo->prioridade = prioridade;
+
+    // insere o atendimento na lista
+    inserir(l, novo);
+
+    // exibe as informações do atendimento
+    printf("Atendimento:\n");
+    printf("Paciente: %s\n", paciente);
+    printf("Medico: %s %s\n", medico, especialidade);
+    printf("Motivo da consulta: %s\n", motivo);
+    printf("Prioridade: %s\n", (prioridade == 1 ? "Leve" : (prioridade == 2 ? "Moderada" : "Grave")));
+}
+
+void bubbleSortPrioridadeCrescente(LISTA *l)
+{
+    if (l == NULL || l->inicio == NULL)
+    {
+        printf("Lista vazia ou nao inicializada!\n");
+        return;
+    }
+
+    int trocado;
+    ITEM *atual;
+    ATENDIMENTO *atendimentoAtual;
+    void *temp;
+
+    do
+    {
+        trocado = 0;
+        atual = l->inicio;
+
+        while (atual->prox != NULL)
+        {
+            ATENDIMENTO *atendimentoAtual = (ATENDIMENTO *)atual->chave;
+            ATENDIMENTO *atendimentoProx = (ATENDIMENTO *)atual->prox->chave;
+
+            if (atendimentoAtual->prioridade > atendimentoProx->prioridade)
+            {
+                // Troca os elementos na lista
+                temp = atual->chave;
+                atual->chave = atual->prox->chave;
+                atual->prox->chave = temp;
+
+                trocado = 1;
+            }
+
+            atual = atual->prox;
+        }
+    } while (trocado);
+}
+
+void bubbleSortPrioridadeDecrescente(LISTA *l)
+{
+    if (l == NULL || l->inicio == NULL)
+    {
+        printf("Lista vazia ou nao inicializada!\n");
+        return;
+    }
+
+    int trocado;
+    ITEM *atual;
+    ATENDIMENTO *atendimentoAtual;
+    void *temp;
+
+    do
+    {
+        trocado = 0;
+        atual = l->inicio;
+
+        while (atual->prox != NULL)
+        {
+            ATENDIMENTO *atendimentoAtual = (ATENDIMENTO *)atual->chave;
+            ATENDIMENTO *atendimentoProx = (ATENDIMENTO *)atual->prox->chave;
+
+            if (atendimentoAtual->prioridade < atendimentoProx->prioridade) // Alteração aqui
+            {
+                // Troca os elementos na lista
+                temp = atual->chave;
+                atual->chave = atual->prox->chave;
+                atual->prox->chave = temp;
+
+                trocado = 1;
+            }
+
+            atual = atual->prox;
+        }
+    } while (trocado);
+}
+
+void listarAtendimentos(LISTA *l)
+{
+    if (l == NULL || l->inicio == NULL)
+    {
+        printf("Nenhum atendimento realizado!\n");
+        return;
+    }
+
+    printf("\nAtendimentos Realizados: ");
+    ITEM *atual = l->inicio;
+    while (atual != NULL)
+    {
+        ATENDIMENTO *atendimento = (ATENDIMENTO *)atual->chave;
+        if (atendimento != NULL)
+        {
+            printf("\nPaciente: %s\n", atendimento->paciente->nome);
+            printf("Medico: %s\t", atendimento->medico->nome);
+            printf("Especialidade: %s\n", atendimento->medico->especialidade);
+            printf("Motivo da consulta: %s\n", atendimento->motivo);
+            printf("Prioridade: %s\n", (atendimento->prioridade == 1 ? "Leve" : (atendimento->prioridade == 2 ? "Moderada" : "Grave")));
+        }
+        atual = atual->prox;
+    }
+}
+
+void editarAtendimento(LISTA *atendimentos)
+{
+    int id;
+    printf("Digite o ID do atendimento que deseja editar: ");
+    scanf("%d", &id);
+    getchar(); // Limpa o buffer
+
+    if (id < 1 || id > atendimentos->qtde)
+    {
+        printf("ID inválido!\n");
+        return;
+    }
+
+    // Encontre o atendimento correspondente na lista
+    ITEM *atual = atendimentos->inicio;
+    for (int i = 1; i < id; i++)
+    {
+        atual = atual->prox;
+    }
+
+    ATENDIMENTO *atendimento = (ATENDIMENTO *)atual->chave;
+
+    printf("Motivo atual: %s\n", atendimento->motivo);
+    printf("Digite o novo motivo: ");
+    fgets(atendimento->motivo, sizeof(atendimento->motivo), stdin);
+    atendimento->motivo[strcspn(atendimento->motivo, "\n")] = '\0';
+
+    printf("Prioridade atual: %d\n", atendimento->prioridade);
+    printf("Digite a nova prioridade: ");
+    scanf("%d", &atendimento->prioridade);
+    getchar(); // Limpa o buffer
+}
+
+void alterarMedicoAtendimento(LISTA *atendimentos, MEDICO *medicos, int qtdeMedicos)
+{
+    if (atendimentos == NULL || atendimentos->inicio == NULL)
+    {
+        printf("Nenhum atendimento cadastrado!\n");
+        return;
+    }
+
+    // Exibe todos os atendimentos para o usuário escolher
+    printf("Atendimentos:\n");
+    ITEM *atual = atendimentos->inicio;
+    for (int i = 1; i <= atendimentos->qtde; i++)
+    {
+        ATENDIMENTO *atendimento = (ATENDIMENTO *)atual->chave;
+        printf("%d - Paciente: %s, Medico: %s\n", i, atendimento->paciente->nome, atendimento->medico->nome);
+        atual = atual->prox;
+    }
+
+    printf("Digite o número do atendimento que deseja alterar o medico: ");
+    int id;
+    scanf("%d", &id);
+    getchar(); // Limpa o buffer
+
+    if (id < 1 || id > atendimentos->qtde)
+    {
+        printf("Número de atendimento inválido!\n");
+        return;
+    }
+
+    // Encontre o atendimento correspondente na lista
+    atual = atendimentos->inicio;
+    for (int i = 1; i < id; i++)
+    {
+        atual = atual->prox;
+    }
+
+    ATENDIMENTO *atendimento = (ATENDIMENTO *)atual->chave;
+
+    // Exibe todos os médicos para o usuário escolher
+    printf("Medicos:\n");
+    for (int i = 0; i < qtdeMedicos; i++)
+    {
+        printf("%d - Nome: %s, Especialidade: %s\n", i + 1, medicos[i].nome, medicos[i].especialidade);
+    }
+
+    printf("Digite o número do novo medico: ");
+    int idMedico;
+    scanf("%d", &idMedico);
+    getchar(); // Limpa o buffer
+
+    if (idMedico < 1 || idMedico > qtdeMedicos)
+    {
+        printf("Número de medico inválido!\n");
+        return;
+    }
+
+    atendimento->medico = &medicos[idMedico - 1];
+    printf("O medico do atendimento foi alterado com sucesso!\n");
+}
+
+void encerrarConsulta(LISTA *atendimentos)
+{
+    int id;
+    printf("Digite o ID do atendimento que deseja encerrar: ");
+    scanf("%d", &id);
+    getchar(); // Limpa o buffer
+
+    if (id < 1 || id > atendimentos->qtde)
+    {
+        printf("ID inválido!\n");
+        return;
+    }
+
+    // Encontre o atendimento correspondente na lista
+    ITEM *atual = atendimentos->inicio;
+    ITEM *anterior = NULL;
+    for (int i = 1; i < id; i++)
+    {
+        anterior = atual;
+        atual = atual->prox;
+    }
+
+    // Remova o atendimento da lista
+    if (anterior == NULL)
+    {
+        atendimentos->inicio = atual->prox;
+    }
+    else
+    {
+        anterior->prox = atual->prox;
+    }
+
+    // Libere a memória alocada para o atendimento
+    free(atual->chave);
+    free(atual);
+
+    atendimentos->qtde--;
+
+    printf("Atendimento encerrado com sucesso!\n");
+}
+
+void salvarAtendimentosEmArquivo(LISTA *atendimentos)
+{
+    FILE *arq = fopen("atendimentos.txt", "w"); // abre ou cria um arquivo txt para escrita.
+
+    if (arq)
+    {
+        ITEM *atual = atendimentos->inicio;
+        while (atual != NULL)
+        {
+            ATENDIMENTO *atendimento = (ATENDIMENTO *)atual->chave;
+            fprintf(arq, "Paciente: %s\n", atendimento->paciente->nome);
+            fprintf(arq, "Idade: %d\n", atendimento->paciente->idade);
+            fprintf(arq, "Medico: %s\n", atendimento->medico->nome);
+            fprintf(arq, "Especialidade: %s\n", atendimento->medico->especialidade);
+            fprintf(arq, "Motivo: %s\n", atendimento->motivo);
+            fprintf(arq, "Prioridade: %d\n\n", atendimento->prioridade);
+            atual = atual->prox;
+        }
+        fclose(arq); // fecha o arquivo.
+    }
+    else
+    {
+        printf("ERRO: nao é possivel abrir o arquivo.");
+    }
+}
+
+void imprimirConteudoDoArquivo(PACIENTE *novoPaciente)
+{
+    FILE *arq = fopen("atendimentos.txt", "r"); // Abre o arquivo em modo de leitura
+
+    if (arq == NULL)
+    {
+        printf("ERRO: nao é possível abrir o arquivo.\n");
+        return;
+    }
+
+    char linha[100]; // Buffer para armazenar cada linha do arquivo
+
+    // Lê cada linha do arquivo até o final
+    while (fgets(linha, sizeof(linha), arq) != NULL)
+    {
+        printf("%s", linha); // Imprime a linha
+    }
+
+    fclose(arq); // Fecha o arquivo
+}
+
+ITEM *buscaSequencialPrioridade(LISTA *atendimentos, int prioridade)
+{
+    if (atendimentos == NULL || atendimentos->inicio == NULL)
+    {
+        printf("Nenhum atendimento cadastrado!\n");
+        return NULL;
+    }
+
+    ITEM *atual = atendimentos->inicio;
+    while (atual != NULL)
+    {
+        ATENDIMENTO *atendimento = (ATENDIMENTO *)atual->chave;
+        if (atendimento->prioridade == prioridade)
+        {
+            return atual;
+        }
+        atual = atual->prox;
+    }
+
+    printf("Atendimento com prioridade %d nao encontrado!\n", prioridade);
+    return NULL;
+}
 
 int main()
 {
@@ -72,6 +598,7 @@ int main()
         printf("3 - Gerenciar Atendimentos\n");
         printf("4 - Salvar ou Ler Arquivo\n");
         printf("5 - Ordenar atendimento\n");
+        printf("6 - Busca\n");
         printf("0 - Sair\n");
         printf("Digite uma opcao: ");
         scanf("%d", &op);
@@ -83,6 +610,7 @@ int main()
             printf("2 - Cadastrar medico\n");
             printf("Digite uma opcao: ");
             scanf("%d", &aux_opc);
+            getchar(); // Limpa o buffer
 
             switch (aux_opc)
             {
@@ -102,7 +630,7 @@ int main()
             break;
         case 2: // LISTAGEM
             printf("1 - Listar Paciente\n");
-            printf("2 - Listar Médico\n");
+            printf("2 - Listar Medico\n");
             printf("Digite uma opcao: ");
             scanf("%d", &aux_opc);
 
@@ -141,10 +669,10 @@ int main()
             case 3:
                 encerrarConsulta(&atendimentos);
                 break;
-            case 4://ALTERAR
+            case 4: // ALTERAR
                 printf("\nQual destes tipos de dados vc quer alterar:\n");
                 printf("1 - Editar Motivo da Consulta e Prioridade.\n");
-                printf("2 - Editar Consulta já agendado com médico.\n");
+                printf("2 - Editar Consulta já agendado com medico.\n");
                 printf("Digite uma opcao: ");
                 scanf("%d", &aux_opc_edit);
                 switch (aux_opc_edit)
@@ -164,7 +692,7 @@ int main()
                 break;
             }
             break;
-        case 4: //ARQUIVOS
+        case 4: // ARQUIVOS
             printf("1 - Salvar em Arquivo\n");
             printf("2 - Ler este Arquivo\n");
             printf("Digite uma opcao: ");
@@ -183,7 +711,7 @@ int main()
                 break;
             }
             break;
-        case 5://ordenacao
+        case 5: // ordenacao
             printf("1 - Ordenar por Prioridade Crescente\n");
             printf("2 - Ordenada por Prioridade Decrescente.\n");
             printf("3 - Ordenar por Motivo alfabeticamente.\n");
@@ -198,7 +726,7 @@ int main()
                 bubbleSortPrioridadeDecrescente(&atendimentos);
                 break;
             case 3:
-                //quickSortMotivo(&atendimentos, );
+                // quickSortMotivo(&atendimentos, );
                 break;
             default:
                 printf("Opcao invalida.\n");
@@ -206,6 +734,34 @@ int main()
             }
             listarAtendimentos(&atendimentos);
             break;
+        case 6:
+            printf("1 - Busca sequencial por prioridade\n");
+            printf("2 - Busca sequencial por motivo da consulta\n");
+            printf("Escolha uma opcao: ");
+            scanf("%d", &aux_opc);
+
+            switch (aux_opc)
+            {
+            case 1:
+                printf("Digite a prioridade buscada: ");
+                scanf("%d", &aux_opc_edit); // vou usar a msm variavel p/ economizar
+                ITEM *auxil;
+                auxil = buscaSequencialPrioridade(&atendimentos, aux_opc_edit);
+                ATENDIMENTO *atendimento = (ATENDIMENTO *)auxil->chave;
+                
+                printf("O primeiro atendimento da lista da prioridade desejada\n");
+                printf("\nPaciente: %s\n", atendimento->paciente->nome);
+                printf("Medico: %s\t", atendimento->medico->nome);
+                printf("Especialidade: %s\n", atendimento->medico->especialidade);
+                printf("Motivo da consulta: %s\n", atendimento->motivo);
+                printf("Prioridade: %s\n", (atendimento->prioridade == 1 ? "Leve" : (atendimento->prioridade == 2 ? "Moderada" : "Grave")));
+                break;
+            case 2:
+                // buscaSequencialMotivo();
+                break;
+            default:
+                break;
+            }
         case 0:
             printf("Saindo do sistema\n");
             break;
